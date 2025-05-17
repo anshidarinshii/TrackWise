20a9d2c5d4e561741e6e7a665fcdfefd516f0fed
anshidarinshii <anshidarinshi@gmail.com>
2025-05-18 00:08:34 +0530

    Change currency from USD to INR

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -155,9 +155,11 @@ function loadTransactions() {
 }
 
 function formatCurrency(amount) {
-    return new Intl.NumberFormat('en-US', {
+    return new Intl.NumberFormat('en-IN', {
         style: 'currency',
-        currency: 'USD'
+        currency: 'INR',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
     }).format(amount);
 }
 
